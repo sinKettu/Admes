8fd9c3757eab18c20ab0fd078929865cee4a3399
SinFox <avangard.jazz@gmail.com>
2019-05-26 23:09:17 +0300

    command size checking

--- main.cpp
+++ main.cpp
@@ -26,43 +26,38 @@ int main(int argc, char *argv[])
             continue;
 
         QStringList commands = input.split(' ');
-        if (commands[0].compare("/open") == 0)
+        if (commands[0].compare("/open") == 0 && commands.length() >= 2)
         {
-            if (commands[1].compare("tor") == 0)
+            if (commands[1].compare("tor") == 0 && commands.length() == 3)
             {
                 c->StartTorServer(commands[2].toUShort());
                 continue;
             }
-            else
+            else if (commands[1].compare("tor") && commands.length() == 2)
             {
                 c->StartServer(commands[1].toUShort());
                 continue;
             }
         }
-        if (commands[0].compare("/connect") == 0)
+        if (commands[0].compare("/connect") == 0 && commands.length() >= 3)
         {
-            if (commands[1].compare("tor") == 0)
+            if (commands[1].compare("tor") == 0 && commands.length() == 4)
             {
                 c->ConnectThroughSOCKS5(commands[2], commands[3].toUShort());
                 continue;
             }
-            else
+            else if (commands[1].compare("tor") && commands.length() == 3)
             {
                 c->ConnectTo(commands[1], commands[2].toUShort());
                 continue;
             }
         }
-        if (commands[0].compare("/disconnect") == 0)
+        if (commands[0].compare("/disconnect") == 0 && commands.length() == 2)
         {
             c->Disconnect(commands[1].toLongLong());
             continue;
         }
-        if (commands[0].compare("/send") == 0)
-        {
-            c->Send(commands[1].toLongLong(), commands[2]);
-            continue;
-        }
-        if (commands.length() == 2 && commands[0].compare("/chat") == 0)
+        if (commands[0].compare("/chat") == 0 && commands.length() == 2)
         {
             std::cout << "\n" << separator;
             std::cout << "admes switched to dialog mode\n";
@@ -77,7 +72,7 @@ int main(int argc, char *argv[])
             c->Send(chatID, input);
             continue;
         }
-        if (commands.length() == 1 && commands[0].compare("/close") == 0)
+        if (commands[0].compare("/close") == 0 && commands.length() == 1)
         {
             chatID = 0;
             c->CloseDialog();
