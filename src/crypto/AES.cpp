9cd05ae9cd1d713d3793b09970c927268a2c1cc3
SinFox <avangard.jazz@gmail.com>
2019-06-06 14:53:30 +0300

    fix

--- src/crypto/AES.cpp
+++ src/crypto/AES.cpp
@@ -1,5 +1,6 @@
 #include <cstring> // memcpy
 #include <string> // std::swap
+#include "AES.h"
 
 typedef unsigned char byte;
 
@@ -570,7 +571,7 @@ bool AES_CBC_Encrypt(byte *input_buffer, uint32_t ib_len,
     return true;
 }
 
-bool AES_CBC_Encrypt(byte *input_buffer, uint32_t ib_len,
+bool AES_CBC_Decrypt(byte *input_buffer, uint32_t ib_len,
                      byte **output_buffer, uint32_t &ob_len,
                      byte *key, uint32_t k_len,
                      byte *iv)
