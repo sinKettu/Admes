fc0e7ea055c852b862c2dd5180e54a48a8830dab
SinFox <avangard.jazz@gmail.com>
2019-06-07 13:58:03 +0300

    renewed

--- src/crypto/prng.h
+++ src/crypto/prng.h
@@ -1,33 +1,24 @@
 #pragma once
-//
-// Created by sin on 24.03.18.
-//
 
 #ifndef OCCULUS_PRNG_H
 #define OCCULUS_PRNG_H
 
-#endif //OCCULUS_PRNG_H
-
+#include "EC_common.h"
 #if defined(_WIN32) || defined(_WIN64)
     #include "libs/mpir/mpir.h"
 #else
     #include <mpir.h>
 #endif
+//
+// PRNG based on ECC
+//
+void EPNG_init(int, mpz_t);
+bool EPNG_inited();
+bool EPNG_get(mpz_t);
+void EPNG_delete();
+//
+// random number based on time of typing
+//
+void typing_random(mpz_t result);
 
-struct Point
-{
-    mpz_t x;
-    mpz_t y;
-};
-//
-// Генератор псевдо-случайных чисел
-// Предположительно, криптостойкий
-//
-    void EPNG_init(int, mpz_t);
-    bool EPNG_inited();
-    bool EPNG_get(mpz_t);
-    void EPNG_delete();
-//
-void simple_numbers_list(int n=100000);
-void typing_random(mpz_t);
-void get_primary_rand(unsigned short, mpz_t, mpz_t, mpz_t, bool );
+#endif //OCCULUS_PRNG_H
