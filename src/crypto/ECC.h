1f2541b8397589ae93ba68a83b1eea6a139c6208
SinFox <avangard.jazz@gmail.com>
2019-06-07 14:29:48 +0300

    ECC_Decrypt signature changed

--- src/crypto/ECC.h
+++ src/crypto/ECC.h
@@ -10,7 +10,7 @@ Keychain *ecc_keygen(EllipticCurve *ec);
 int ECC_Encrypt(EllipticCurve *ec, Point pk, unsigned char *message, unsigned int m_len, ECC_encrypted_data *data);
 
 // Расшифрование сообщения
-int ECC_Decrypt(EllipticCurve *ec, mpz_t prk, ECC_encrypted_data data, unsigned char **message, unsigned int &m_len);
+int ECC_Decrypt(EllipticCurve *ec, mpz_t prk, ECC_encrypted_data *data, unsigned char **message, unsigned int &m_len);
 
 // Создание цифровой подписи
 // ГОСТ 34.10-2012
