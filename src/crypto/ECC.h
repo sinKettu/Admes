8f51a492211c2cbee1fd7c920981d42aba58f8cd
SinFox <avangard.jazz@gmail.com>
2019-06-07 13:43:01 +0300

    corrections

--- src/crypto/ECC.h
+++ src/crypto/ECC.h
@@ -2,76 +2,28 @@
 //
 // Created by sin on 06.11.18.
 //
-#include "prng.h" // structure Point is defined here
+#include "prng.h" 
+#include "EC_common.h"
 
 #ifndef OCCULUS_ECC_H
 #define OCCULUS_ECC_H
 
-// Структура - зашифрованное с помощью ЭК сообщение
-// От точек ЭК берутся только иксы
-struct ecc_encrypted_data
-{
-    unsigned char* side_x;
-    unsigned char* side_y;
-    unsigned char* general_x;
-    unsigned char* general_y;
-    int s_len_x;
-    int s_len_y;
-    int g_len_x;
-    int g_len_y;
-};
-
-// Обозначения согласно
-// ГОСТ 34.10-2012
-struct ecc_signature
-{
-    unsigned char* r;
-    unsigned char* s;
-    int r_len;
-    int s_len;
-};
-
-// Инициализация параметров для current_elliptic_curve
-// С которой в дальнейшем будет работать алгоритм
-// 0 - Успех
-// 1 - слишком большое num
-// 2 - параметры уже инициализованы, сначала нужно удалить кривую
-int ecc_init_params(unsigned char num);
-
-// Удаление параметров ЭК из памяти
-// 0 - Успех
-// 1 - параметры не инициализированы
-int ecc_delete_params();
-
 // Генерация публичного и закрытого ключей
-void ecc_keygen();
+Keychain *ecc_keygen(EllipticCurve *ec);
 
 // Шифрование сообщения
-// 0 - Успех
-// 1 - Ключ не сгенерирован/считан
-// 2 - Размер сообщения большего модуля ЭК
-int ecc_encrypt(unsigned char* message, int m_len, ecc_encrypted_data &data);
+int ECC_Encrypt(EllipticCurve *ec, Point pk, unsigned char *message, unsigned int m_len, ECC_encrypted_data *data);
 
 // Расшифрование сообщения
-// 0 - Успех
-// 1 - Ключ не сгенерирован/считан
-// 2 - Не инициализирована ЭК
-int ecc_decrypt(ecc_encrypted_data data, unsigned char** message, int &m_len);
+int ECC_Decrypt(EllipticCurve *ec, mpz_t prk, ECC_encrypted_data data, unsigned char **message, unsigned int &m_len);
 
 // Создание цифровой подписи
 // ГОСТ 34.10-2012
-// 0 - Успех
-// 1 - Не ииницилизирована ЭК
-// 2 - Ключ не сгенерирован/считан
-int ecc_make_sign(unsigned char* message, int m_len, ecc_signature &signature);
+int ECC_Sign(EllipticCurve *ec, mpz_t prk, unsigned char *message, unsigned int m_len, ECC_signature *signature);
 
 // Проверка цифровой подписи
 // ГОСТ 34.10-2012
-// 0 - Проверка удалась
-// 1 - Проверка не удалась
-// 2 - Не инициализирована ЭК
-// 3 - Ключ не сгенерирован/считан
-int ecc_check_sign(ecc_signature signature, unsigned char* hash, int h_len);
+int ecc_check_sign(EllipticCurve *ec, Point pk, ECC_signature *signature, unsigned char* hash, int h_len);
 
 void ecc_test();
 
