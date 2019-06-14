aa6ba288f623724939d2c6c875fe6f4af9631c5c
SinFox <avangard.jazz@gmail.com>
2019-06-14 10:40:40 +0300

    TODO

--- src/crypto/AES.h
+++ src/crypto/AES.h
@@ -9,3 +9,6 @@ QByteArray AES_CBC_Encrypt(QByteArray buffer, char *key, unsigned int key_len, u
 QByteArray AES_CBC_Decrypt(QByteArray encrypted, char *key, unsigned int key_len, unsigned char *iv);
 
 // void AES_test();
+
+// TODO:
+//      'RoundKeysInstall' function for optimizing
